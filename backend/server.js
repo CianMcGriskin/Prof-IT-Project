0005ca3b34cb45ecaf032f0169a1a4dda5cf59bd
Maciej <g00391712@atu.ie>
2023-02-26 21:50:53 +0000

    TimeTable fetch working, other things wip

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const uuid = require('uuid');
+const uuid = require("uuid");
 const app = express();
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 // Set up body-parser and cors middleware
 app.use(bodyParser.json());
@@ -12,50 +12,33 @@ app.use(cors());
 app.use(cookieParser());
 
 // Connect to MongoDB database
-mongoose.connect("mongodb+srv://batman:root@cluster0.tjfhrts.mongodb.net/Rosterota?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://batman:root@cluster0.tjfhrts.mongodb.net/Rosterota?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 // Schemas of the backend
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-}, { collection: "Users" });
+const userSchema = new mongoose.Schema(
+  {
+    email: String,
+    password: String,
+  },
+  { collection: "Users" }
+);
 
+// Define Mongoose schema
 const hoursSchema = new mongoose.Schema({
+  schedule: [mongoose.Schema.Types.Mixed],
   userID: String,
   weekID: String,
-  schedule: Array
-});
+},
+{ collection: "Hours" });
 
 const Users = mongoose.model("User", userSchema);
-const Hours = mongoose.model('Hours', hoursSchema);
-
-app.get('/timetable', (req, res) => {
-  let userID = "1";
-  let weekID = "1";
-
-  // Find the 'hours' document for the specified user ID
-  Hours.findOne({ userID: userID, weekID: weekID}, (err, hours) => {
-    if (err) 
-      console.log("Error");
-    
-    if (!hours) 
-      console.log("Finding UserID & WeekID returned null");
-
-    console.log(hours);
-
-    // Find the schedule data for the specified week ID
-    const schedule = hours.schedule;
-    if (!schedule) 
-      console.log("scheudle not found");
-    
-    console.log(schedule);
-    // Return the schedule data for the specified week ID
-    return res.json(schedule);
-  });
-});
+const Hours = mongoose.model("Hours", hoursSchema);
 
 // Set up a login API endpoint
 app.post("/", (req, res) => {
@@ -67,13 +50,13 @@ app.post("/", (req, res) => {
       res.status(500).send("Server error");
     } else if (!user) {
       res.status(400).send("Email not found");
-      console.log("Email not found")
+      console.log("Email not found");
     } else {
       // Check if password matches
       if (user.password === password /* && user.status === "Accepted" */) {
-        res.cookie('UserAuth', 'AuthTest', { httpOnly: false });
+        res.cookie("UserAuth", "AuthTest", { httpOnly: false });
         res.status(200).send("success");
-        console.log("Correct password and user is accepted")
+        console.log("Correct password and user is accepted");
       } else if (user.password === password && user.status != "Accepted") {
         res.status(400).send("User not accepted");
         console.log("User not accepted");
@@ -84,6 +67,35 @@ app.post("/", (req, res) => {
     }
   });
 });
+
+app.get("/timetable", async (req, res) => {
+  try {
+    // Fetch data from "Hours" collection
+    const hours = await Hours.find();
+    // Return data as JSON response
+    res.json(hours);
+  } catch (err) {
+    // Handle error
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
+
+app.get("/timetable/:weekId", async (req, res) => {
+  try {
+    const weekId = req.params.weekId;
+    // Fetch data from "Hours" collection for the specified week
+    const timetable = await Hours.findOne({ weekID: weekId });
+    // Return data as JSON response
+    res.json(timetable);
+  } catch (err) {
+    // Handle error
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
+
+
 
 // Start the server
 let port = 4000;
--- src/components/Timetable/Timetable.js
+++ src/components/Timetable/Timetable.js
@@ -1,23 +1,65 @@
 import NavigationBar from '../Navbar/Navbar'
 import axios from 'axios';
-
-
+import { useState } from 'react';
 
 const TimetablePage = () => {
-  axios.get('http://localhost:4000/timetable')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+  const [selectedWeek, setSelectedWeek] = useState("1"); // default to week 1
+  const [timetable, setTimetable] = useState(null);
 
-    return (
+  const handleWeekChange = (event) => {
+    setSelectedWeek(event.target.value);
+    axios.get(`http://localhost:4000/timetable/${event.target.value}`)
+      .then(response => {
+        setTimetable(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  return (
+    <div>
+      <NavigationBar/>
+      <div>
+        <label htmlFor="week-select">Select week:</label>
+        <select id="week-select" value={selectedWeek} onChange={handleWeekChange}>
+          <option value="1">Week 1</option>
+          <option value="2">Week 2</option>
+          <option value="3">Week 3</option>
+          {/* Add more options for additional weeks */}
+        </select>
+      </div>
+      {timetable ? (
         <div>
-        <NavigationBar/>
-        Timetable
+          {/* Log the timetable data */}
+          {console.log(timetable)}
+          <table>
+            <thead>
+              <tr>
+                <th>Day</th>
+                <th>Start time</th>
+                <th>End time</th>
+                <th>Hours</th>
+              </tr>
+            </thead>
+            <tbody>
+              {timetable.schedule.map((entry, index) => (
+                <tr key={index}>
+                  <td>{entry[0]}</td>
+                  <td>{entry[1]}</td>
+                  <td>{entry[2]}</td>
+                  <td>{entry[3]}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-      );
+      ) : (
+        <p>Select a week to view timetable</p>
+      )}
+    </div>
+  );
+  
 };
 
 export default TimetablePage;
